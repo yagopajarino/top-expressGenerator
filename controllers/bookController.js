b15e56baee5e18bfac1d3e850911555d9dc00917
Yago Pajariño <yagopajarino@outlook.com>
2022-06-27 17:19:06 -0300

    Add book list view

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -38,7 +38,15 @@ exports.index = function (req, res) {
 };
 
 exports.book_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: Book list");
+  Book.find({}, "title author")
+    .sort({ title: 1 })
+    .populate("author")
+    .exec(function (err, list_books) {
+      if (err) {
+        return async.nextTick(err);
+      }
+      res.render("book_list", { title: "Book List", book_list: list_books });
+    });
 };
 
 exports.book_detail = function (req, res) {
