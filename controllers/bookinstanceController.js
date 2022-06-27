4bd3ded9aaf20628fbe51f432719d7795918aa81
Yago Pajariño <yagopajarino@outlook.com>
2022-06-27 17:23:12 -0300

    Add book instance view

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,7 +1,18 @@
+const { nextTick } = require("async");
 var BookInstance = require("../models/bookinstance");
 
 exports.bookinstance_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance list");
+  BookInstance.find()
+    .populate("book")
+    .exec(function (err, list) {
+      if (err) {
+        return nextTick(err);
+      }
+      res.render("bookinstance_list", {
+        title: "Book Instance List",
+        bookinstance_list: list,
+      });
+    });
 };
 
 exports.bookinstance_detail = function (req, res) {
