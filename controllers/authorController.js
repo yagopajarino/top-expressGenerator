a5c6fe96beb76773ebe6e38a418185390bad9606
Yago Pajariño <yagopajarino@outlook.com>
2022-06-29 12:42:30 -0300

    Add delete author functionality

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -96,12 +96,63 @@ exports.author_create_post = [
   },
 ];
 
-exports.author_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: author delete GET");
+exports.author_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      author_books: function (callback) {
+        Book.find({ author: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        res.redirect("/catalog/authors");
+      }
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.author_books,
+      });
+    }
+  );
 };
 
-exports.author_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: author delete POST");
+exports.author_delete_post = function (req, res, next) {
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.body.authorid).exec(callback);
+      },
+      author_books: function (callback) {
+        Book.find({ author: req.body.authorid }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author_books.length > 0) {
+        res.render("author_delete", {
+          title: "Delete Author",
+          author: results.author,
+          author_books: results.author_books,
+        });
+        return;
+      } else {
+        Author.findByIdAndRemove(req.body.authorid, function (err) {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/catalog/authors");
+        });
+      }
+    }
+  );
 };
 
 exports.author_update_get = function (req, res) {
