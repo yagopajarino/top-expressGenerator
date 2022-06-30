d579be8150dfd664ca7d57ecd0119e0ae57a3393
Yago Pajariño <yagopajarino@outlook.com>
2022-06-30 11:31:36 -0300

    Add update author functionality

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -155,10 +155,58 @@ exports.author_delete_post = function (req, res, next) {
   );
 };
 
-exports.author_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: author update GET");
+exports.author_update_get = function (req, res, next) {
+  Author.findById(req.params.id).exec(function (err, author) {
+    if (err) {
+      return next(err);
+    }
+    res.render("author_form", { title: "Update Author", author: author });
+  });
 };
 
-exports.author_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: author update POST");
-};
+exports.author_update_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric("en-US", { ignore: " " })
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric("en-US", { ignore: " " })
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth").isISO8601().toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Update Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      let author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+        _id: req.params.id,
+      });
+      Author.findByIdAndUpdate(req.params.id, author, {}, function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(author.url);
+      });
+    }
+  },
+];
--- routes/catalog.js
+++ routes/catalog.js
@@ -23,8 +23,8 @@ router.get("/author/create", authorController.author_create_get);
 router.post("/author/create", authorController.author_create_post);
 router.get("/author/:id/delete", authorController.author_delete_get);
 router.post("/author/:id/delete", authorController.author_delete_post);
-router.get("/author/:id/update", authorController.author_delete_get);
-router.post("/author/:id/update", authorController.author_delete_post);
+router.get("/author/:id/update", authorController.author_update_get);
+router.post("/author/:id/update", authorController.author_update_post);
 router.get("/author/:id", authorController.author_details);
 router.get("/authors", authorController.author_list);
 
